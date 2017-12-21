94a3af1f7503c42848f2c3946307036b28917dbe
Kelvin Jackson <kechpaja@comcast.net>
2017-12-21 18:19:43 -0500

    Message when new user joins server

--- server/src/core/Router.java
+++ server/src/core/Router.java
@@ -11,9 +11,21 @@ public class Router {
 		this.data_ = new ConcurrentHashMap<String, Vector<String>>();
 	}
 	
+	private void addUser(String user) {
+		String userJoinedMessage = "admin:admin:admin:admin:";
+		userJoinedMessage += "User\\s\"" + user + "\"\\sjoined\\sthe\\sserver!:"; // TODO localize this string! Possibly on client. 
+		userJoinedMessage += String.join(":", this.data_.keySet());
+			
+		for (Vector<String> messageList : this.data_.values()) {
+			messageList.add(userJoinedMessage);
+		}
+	}
+	
 	public void routeMessageToUser(String message, String user) {
-		if (!this.data_.containsKey(user)) {
-			this.data_.put(user, new Vector<String>());
+		synchronized (this.data_) {
+			if (!this.data_.containsKey(user)) {
+				this.data_.put(user, new Vector<String>());
+			}
 		}
 		
 		Vector<String> messageList = this.data_.get(user);
@@ -31,6 +43,13 @@ public class Router {
 			synchronized (messageList) {
 				while (messageList.size() > 0) {
 					messages.add(messageList.remove(0));
+				}
+			}
+		} else {
+			synchronized (this.data_) {
+				if (!this.data_.containsKey(user)) {
+					this.data_.put(user, new Vector<String>());
+					addUser(user);
 				}
 			}
 		}
